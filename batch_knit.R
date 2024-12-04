aff15c833848b533d4790a92ee100d6c182e694b
NachoPerezBlasco <NachoPerezBlasco@users.noreply.github.com>
2024-12-04 10:22:52 +0000

    deploy: 2baf1cbbd206cfb23d627d66b63ef9eb4d4d4cd5

---
+++ batch_knit.R
@@ -0,0 +1,20 @@
+
+# Load the rmarkdown package
+
+library(rmarkdown)
+
+# List of R Markdown files
+files <- c("index.Rmd",
+           "appfeatures.Rmd", 
+           "package.Rmd",
+           "timetoevent_vignette.Rmd",
+           "binary_vignette.Rmd", 
+           "simulations_vignette.Rmd", 
+           "aboutus.Rmd", 
+           "publications.Rmd", 
+           "talks_1.Rmd")
+
+# Knit each file to HTML
+for (file in files) {
+  render(input = file)
+}
