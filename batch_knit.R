cce8aa4792bc9268e88ab72de56d95244d9e11e2
Nacho <ignacio.perez.blasco@upc.edu>
2024-12-04 10:25:31 +0100

    changes

---
+++ batch_knit.R
@@ -0,0 +1,24 @@
+
+knitr::opts_knit$set(root.dir = rprojroot::find_rstudio_root_file())
+
+# Load the rmarkdown package
+
+library(rmarkdown)
+
+# List of R Markdown files
+files <- c("index.Rmd", 
+           "appfeatures.Rmd", 
+           "package.Rmd",
+           "timetoevent_vignette.Rmd", 
+           "binary_vignette.Rmd", 
+           "simulations_vignette.Rmd",
+           "aboutus.Rmd",
+           "publications.Rmd",
+           "talks_1.Rmd",
+           "contact.Rmd"
+           ) 
+
+# Knit each file to HTML
+for (file in files) {
+  render(input = file, output_format = "html_document")
+}
